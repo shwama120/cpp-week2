e4a4e9871a607a7c1f620a06022bee42fb94dbe4
Sungjin <truesj1020@gmail.com>
2024-03-27 00:02:52 +0900

    fix: 숫자가 랜덤으로 출력되도록 설정

--- util.cpp
+++ util.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <random>  
-
+#include <chrono>
 
 int util(int answer) {
 
-    std::random_device rand; 
-    std::mt19937 gen(rand()); 
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();  
+    std::mt19937 gen(seed); 
 
     std::uniform_int_distribution<> dis(100, 1000); 
 
